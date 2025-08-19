18c1a35fd9fbc600c2b92aaac9a886667be62211
charchar111 <3hjab@naver.com>
2025-08-20 07:29:56 +0900

    주석, ui 문구 수정

--- page/sync/sync-app.js
+++ page/sync/sync-app.js
@@ -1,6 +1,9 @@
 let count = 0;
 
 // 카운터 로직
+// trigger - 버튼 클릭
+// action - 카운트 증가, 콘솔 출력
+// render - 카운트 증가 값 출력
 (function initButtonCountAdd() {
   const buttonCountAdd = document.querySelector("#button_count_add");
   const countConsole = document.querySelector("#count_console");
@@ -15,10 +18,11 @@ let count = 0;
 })();
 
 // 버튼 - 무거운 재귀 작업 테스트
+// trigger - 버튼 클릭
+// action - 동기적으로 재귀 함수 호출, 매 호출마다 콘솔 출력
 (function initButtonRecursive() {
   const buttonRecursive = document.querySelector("#button_recursive");
 
-  // 동기 재귀
   function syncRecursive(n, index = 0) {
     console.log("syncRecursive");
     if (index < n) syncRecursive(n, index + 1);
@@ -29,10 +33,11 @@ let count = 0;
 })();
 
 // 버튼 - 무거운 반복 작업 테스트
+// trigger - 버튼 클릭
+// action - 동기적으로 반복문 실행, 매 반복마다 콘솔 출력
 (function initButtonLoof() {
   const buttonLoof = document.querySelector("#button_loof");
 
-  // 동기 반복
   function syncLoof(n) {
     let i = 0;
 
